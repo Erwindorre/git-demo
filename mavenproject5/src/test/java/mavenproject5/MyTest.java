3650bb342f5cbed0addce5448b5ad7790b37f5cf
closesure <closesure@MacBook-Pro-van-closesure-2.local>
2019-11-12 19:05:02 +0100

    doe maar wat

---
+++ mavenproject5/src/test/java/mavenproject5/MyTest.java
@@ -0,0 +1,18 @@
+package mavenproject5;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+	
+	@Test
+	void test2() {
+		assertTrue(true);
+	}
+}
